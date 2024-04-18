1522139e0a60780bddc7bff250941011c571ff34
Saurabh Sharma <saurabh11sharma@live.com>
2024-04-18 20:54:34 +0530

    auth update final

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { currentUser } from './decorators/current-user.decorator';
+import { currentUser } from '@app/common';
 import { UserDocument } from './users/models/user.schema';
 import { Response } from 'express';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller()
@@ -23,7 +23,7 @@ export class AuthController {
 
   @UseGuards(JwtAuthGuard)
   @MessagePattern('authenticate')
-  async authenticate(user: UserDocument) {
-    return user;
+  async authenticate(@Payload() data: any) {
+    return data.user;
   }
 }
